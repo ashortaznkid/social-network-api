52e00117c999e73161a649b91f5ef27c4f4ab0e9
ashortaznkid <mccue.nguyen@gmail.com>
2023-09-14 19:42:15 -0700

    fixes

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 const { connect, connection } = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Cluster1');
+
+connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Cluster1');
 
 
 module.exports = connection;
