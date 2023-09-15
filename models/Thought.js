8e9b6b7907fd69d203abcdba012670e19ab34867
ashortaznkid <mccue.nguyen@gmail.com>
2023-09-14 19:46:01 -0700

    thought.js fixed

--- models/Thought.js
+++ models/Thought.js
@@ -1,35 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
 
-const thoughtSchema = new Schema (
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlenght: 1,
-            maxlenght: 280,
-        },
-
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            //get: createdAtVal 
-        },
-
-        username: {
-            type: String,
-            required: true,
-        },
-
-        reactions: [reactionSchema],
-    },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        id: false,
-    }
-)
-
 const reactionSchema = new Schema (
     {
         reactionId: {
@@ -61,6 +31,38 @@ const reactionSchema = new Schema (
         id: false,
     }
 )
+
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlenght: 1,
+            maxlenght: 280,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            //get: createdAtVal 
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        reaction: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+)
+
 
 thoughtSchema.virtuals('reactionCount').get(function() {
     return this.reactions.length;
